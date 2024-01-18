7bb490a3f4321ae4a55c4477b8a88609132da979
parkshinya <154995848+parkshinya@users.noreply.github.com>
2024-01-18 22:26:11 +0900

    create side-nav

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import { ConfigProvider } from "antd";
+import type { ThemeConfig } from "antd";
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
+// const config: ThemeConfig = {
+//   token: {
+//     colorBgBase: "",
+//     colorBgContainer: "",
+//     colorPrimary: "",
+//   },
+// };
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      {/* <ConfigProvider theme={config}> */}
       <App />
+      {/* </ConfigProvider> */}
     </Provider>
   </React.StrictMode>
 );
