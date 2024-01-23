4981e0e79d02d52fb82d2e8da61db47cf76f5827
parkshinya <154995848+parkshinya@users.noreply.github.com>
2024-01-23 09:20:49 +0900

    implement new user create

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -1,64 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, Input } from "antd";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
 import { createUserWithEmailAndPassword } from "@firebase/auth";
-
-const onFinish = async (newUser: FieldType) => {
-  await createUserWithEmailAndPassword(
-    auth,
-    newUser.email,
-    newUser.password
-  ).catch((err) => alert(err.message));
-};
-
-const onFinishFailed = (errorInfo: any) => {
-  console.log("Failed:", errorInfo);
-};
+import { collection, addDoc } from "firebase/firestore";
 
 type FieldType = {
+  username: string;
+  company: string;
   email: string;
   password: string;
 };
 
-const Admin: React.FC = () => (
-  <div style={{ height: "100vh", width: "100%", position: "relative" }}>
-    <Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      style={{
-        maxWidth: 600,
-        position: "absolute",
-        top: "40%",
-        left: "40%",
-        textAlign: "right",
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item<FieldType>
-        label="email"
-        name="email"
-        rules={[{ required: true, message: "Please input your email!" }]}
-      >
-        <Input type="email" />
-      </Form.Item>
+const Admin: React.FC = () => {
+  const [loading, setLoading] = useState(false);
 
-      <Form.Item<FieldType>
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: "Please input your password!" }]}
+  const onFinish = async (newUser: FieldType) => {
+    setLoading(true);
+    await createUserWithEmailAndPassword(auth, newUser.email, newUser.password)
+      .then(async (userCredential) => {
+        await addDoc(collection(db, "users"), {
+          uid: userCredential.user.uid,
+          company: newUser.company,
+          username: newUser.username,
+        });
+      })
+      .then(() => setLoading(false))
+      .catch((err) => alert(err.message));
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <div style={{ height: "100vh", width: "100%", position: "relative" }}>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{
+          maxWidth: 600,
+          position: "absolute",
+          top: "40%",
+          left: "40%",
+          textAlign: "right",
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Input.Password />
-      </Form.Item>
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Register
-        </Button>
-      </Form.Item>
-    </Form>
-  </div>
-);
+        <Form.Item<FieldType>
+          label="username"
+          name="username"
+          rules={[{ required: true, message: "Please input your username" }]}
+        >
+          <Input type="text" />
+        </Form.Item>
+        <Form.Item<FieldType>
+          label="company"
+          name="company"
+          rules={[{ required: true, message: "Please input your company" }]}
+        >
+          <Input type="text" />
+        </Form.Item>
+        <Form.Item<FieldType>
+          label="email"
+          name="email"
+          rules={[{ required: true, message: "Please input your email!" }]}
+        >
+          <Input type="email" />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit" loading={loading}>
+            Register
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
 
 export default Admin;
